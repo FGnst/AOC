d1c7acccd589ab99f57f43d2c9bb6569cc5c2deb
FedericoGnesotto <federico.gnesotto@brainlab.com>
2021-12-27 20:19:13 +0100

    Fix trafos Add data read Add setVectors

--- 2021/src/Day19_BeaconScanner.cpp
+++ 2021/src/Day19_BeaconScanner.cpp
@@ -13,7 +13,6 @@
 #include <vector>
 #include <stack>
 
-
 struct Vector {
     Vector(int x, int y, int z) {
         _x = x;
@@ -33,6 +32,9 @@ struct Vector {
         return Vector{ _x - right._x,  _y - right._y, _z - right._z };
     }
 
+    bool operator==(const Vector& vec) {
+        return vec._x == this->_x && vec._y == this->_y && vec._z == this->_z;
+    }
 };
 
 struct Point {
@@ -53,69 +55,126 @@ struct Point {
     Vector operator-(const Point& right) {
         return Vector{ _x - right._x,  _y - right._y, _z - right._z };
     }
-};
 
+};
 
 struct Scanner {
     std::vector<Point> _points;
     std::vector<Vector> _vectorsBetweenPoints;
 };
 
-std::vector<std::function<Point(Point&)>> trafos{
-    [](const Point& p) {return Point{p._x, p._y, p._z}; }, // 1 x 
-    [](const Point& p) {return Point{p._x, -p._y, -p._z}; }, // 2 
-    [](const Point& p) {return Point{p._x, p._z, -p._y}; }, // 3 
-    [](const Point& p) {return Point{p._x, -p._z, p._y}; }, // 4 
-    [](const Point& p) {return Point{-p._x, p._y, -p._z}; }, // 5 -x
-    [](const Point& p) {return Point{-p._x, -p._y, p._z}; }, // 6 
-    [](const Point& p) {return Point{-p._x, p._z, p._y}; }, // 7 
-    [](const Point& p) {return Point{-p._x, -p._z, -p._y}; }, // 8 
-    [](const Point& p) {return Point{p._y, -p._x, p._z}; }, // 9 y
-    [](const Point& p) {return Point{p._y, p._z, p._x}; }, // 10 
-    [](const Point& p) {return Point{p._y, -p._x, p._z}; }, // 11
-    [](const Point& p) {return Point{p._y, p._x, -p._z}; }, // 12 
-    [](const Point& p) {return Point{-p._y, -p._x, -p._z}; }, // 13 -y
-    [](const Point& p) {return Point{-p._y, p._z, p._x}; }, // 14 
-    [](const Point& p) {return Point{-p._y, -p._z, p._x}; }, // 15 
-    [](const Point& p) {return Point{-p._y, p._x, p._z}; }, // 16 
-    [](const Point& p) {return Point{p._z, p._y, -p._x}; }, // 17 z 
-    [](const Point& p) {return Point{p._z, -p._x, -p._y}; }, // 18 
-    [](const Point& p) {return Point{p._z, -p._y, p._x}; }, // 19 
-    [](const Point& p) {return Point{p._z, p._x, -p._y}; }, // 20 
-    [](const Point& p) {return Point{-p._z, p._y, p._x}; }, // 21 -z
-    [](const Point& p) {return Point{-p._z, -p._y, -p._x}; }, // 22 
-    [](const Point& p) {return Point{-p._z, p._x, -p._y}; }, // 23 
-    [](const Point& p) {return Point{-p._z, -p._x, p._y}; }, // 24 
+std::vector<std::function<Vector(Vector&)>> trafos{
+    [](const Vector& p) {return Vector{p._x, p._y, p._z}; }, // 0
+    [](const Vector& p) {return Vector{p._x, -p._y, -p._z}; }, // 1 
+    [](const Vector& p) {return Vector{p._x, p._z, -p._y}; }, // 2
+    [](const Vector& p) {return Vector{p._x, -p._z, p._y}; }, // 3 
+    [](const Vector& p) {return Vector{-p._x, p._y, -p._z}; }, // 4 -x
+    [](const Vector& p) {return Vector{-p._x, -p._y, p._z}; }, // 5 
+    [](const Vector& p) {return Vector{-p._x, p._z, p._y}; }, // 6 
+    [](const Vector& p) {return Vector{-p._x, -p._z, -p._y}; }, // 7 
+    [](const Vector& p) {return Vector{p._y, -p._x, p._z}; }, // 8 y
+    [](const Vector& p) {return Vector{p._y, p._z, p._x}; }, // 9 
+    [](const Vector& p) {return Vector{p._y, p._x, -p._z}; }, // 10
+    [](const Vector& p) {return Vector{p._y, -p._z, -p._x}; }, // 11 
+    [](const Vector& p) {return Vector{-p._y, -p._x, -p._z}; }, // 12 -y
+    [](const Vector& p) {return Vector{-p._y, p._z, -p._x}; }, // 13 
+    [](const Vector& p) {return Vector{-p._y, -p._z, p._x}; }, // 14 
+    [](const Vector& p) {return Vector{-p._y, p._x, p._z}; }, // 15 
+    [](const Vector& p) {return Vector{p._z, p._y, -p._x}; }, // 16 z 
+    [](const Vector& p) {return Vector{p._z, -p._x, -p._y}; }, // 17 
+    [](const Vector& p) {return Vector{p._z, -p._y, p._x}; }, // 18 
+    [](const Vector& p) {return Vector{p._z, p._x, p._y}; }, // 19 
+    [](const Vector& p) {return Vector{-p._z, p._y, p._x}; }, // 20 -z
+    [](const Vector& p) {return Vector{-p._z, -p._y, -p._x}; }, // 21 
+    [](const Vector& p) {return Vector{-p._z, p._x, -p._y}; }, // 22
+    [](const Vector& p) {return Vector{-p._z, -p._x, p._y}; }, // 23 
 };
 
-std::vector<std::function<Point(Point&)>> invTrafos{
-    [](const Point& p) {return Point{p._x, p._y, p._z}; }, // 1 xyz 
-    [](const Point& p) {return Point{p._x, -p._y, -p._z}; }, // 2 
-    [](const Point& p) {return Point{p._x, -p._z, p._y}; }, // 3 
-    [](const Point& p) {return Point{p._x, p._z, -p._y}; }, // 4 
-    [](const Point& p) {return Point{-p._x, p._y, -p._z}; }, // 5 -xzy
-    [](const Point& p) {return Point{-p._x, -p._y, p._z}; }, // 6 
-    [](const Point& p) {return Point{-p._x, p._z, p._y}; }, // 7 
-    [](const Point& p) {return Point{-p._x, -p._z, -p._y}; }, // 8 
-    [](const Point& p) {return Point{-p._y, p._x, p._z}; }, 
-    [](const Point& p) {return Point{p._z, p._x, p._y}; }, 
-    [](const Point& p) {return Point{p._y, p._x, -p._z}; }, 
-    [](const Point& p) {return Point{-p._z, p._x, -p._y}; }, 
-    [](const Point& p) {return Point{-p._y, -p._x, -p._z}; }, // 9 yxz
-    [](const Point& p) {return Point{-p._z, -p._x, p._y}; }, // 10 
-    [](const Point& p) {return Point{p._z, -p._x, -p._y}; }, // 11
-    [](const Point& p) {return Point{p._y, -p._x, p._z}; }, // 12 
-    [](const Point& p) {return Point{-p._z, p._y, p._x}; }, // 13 yzx
-    [](const Point& p) {return Point{-p._y, -p._z, p._x}; }, // 14 
-    [](const Point& p) {return Point{p._z, -p._y, p._x}; }, // 15 
-    [](const Point& p) {return Point{p._y, p._z, p._x}; }, // 16 
-    [](const Point& p) {return Point{p._z, p._y, -p._x}; }, // 17 zxy 
-    [](const Point& p) {return Point{-p._z, -p._y, -p._x}; }, // 18 
-    [](const Point& p) {return Point{p._y, -p._z, -p._x}; }, // 19 
-    [](const Point& p) {return Point{-p._y, p._z, -p._x}; }, // 20 
+std::vector<std::function<Vector(Vector&)>> invTrafos{
+    [](const Vector& p) {return Vector{p._x, p._y, p._z}; }, // 0
+    [](const Vector& p) {return Vector{p._x, -p._y, -p._z}; }, // 1 
+    [](const Vector& p) {return Vector{p._x, -p._z, p._y}; }, // 2
+    [](const Vector& p) {return Vector{p._x, p._z, -p._y}; }, // 3 
+    [](const Vector& p) {return Vector{-p._x, p._y, -p._z}; }, // 4 -xzy
+    [](const Vector& p) {return Vector{-p._x, -p._y, p._z}; }, // 5 
+    [](const Vector& p) {return Vector{-p._x, p._z, p._y}; }, // 6 
+    [](const Vector& p) {return Vector{-p._x, -p._z, -p._y}; }, // 7 
+    [](const Vector& p) {return Vector{-p._y, p._x, p._z}; }, // 8
+    [](const Vector& p) {return Vector{p._z, p._x, p._y}; }, // 9
+    [](const Vector& p) {return Vector{p._y, p._x, -p._z}; }, // 10
+    [](const Vector& p) {return Vector{-p._z, p._x, -p._y}; }, //11
+    [](const Vector& p) {return Vector{-p._y, -p._x, -p._z}; }, //12
+    [](const Vector& p) {return Vector{-p._z, -p._x, p._y}; }, //13
+    [](const Vector& p) {return Vector{p._z, -p._x, -p._y}; }, //14
+    [](const Vector& p) {return Vector{p._y, -p._x, p._z}; }, //15
+    [](const Vector& p) {return Vector{-p._z, p._y, p._x}; }, //16
+    [](const Vector& p) {return Vector{-p._y, -p._z, p._x}; }, //17
+    [](const Vector& p) {return Vector{p._z, -p._y, p._x}; }, //18
+    [](const Vector& p) {return Vector{p._y, p._z, p._x}; }, //19
+    [](const Vector& p) {return Vector{p._z, p._y, -p._x}; }, //20
+    [](const Vector& p) {return Vector{-p._z, -p._y, -p._x}; }, //21
+    [](const Vector& p) {return Vector{p._y, -p._z, -p._x}; }, //22
+    [](const Vector& p) {return Vector{-p._y, p._z, -p._x}; }, //23
 };
+
+void testInverse() {
+    Vector v{ 2, -3, 1 };
+    for (unsigned i = 0; i < trafos.size(); ++i)
+    {
+        assert(invTrafos[i](trafos[i](v)) == v);
+    }
+}
+
+std::vector<Scanner> readData(const std::string& fileName) {
+    std::fstream f(fileName);
+    std::vector<Scanner> scanners;
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(f, line)) {
+        lines.push_back(line);
+    }
+
+    Scanner s;
+    for (const auto& l : lines) {
+        if (l.empty()) {
+            scanners.push_back(s);
+        }
+        else if (l.substr(0, 3) == "---") {
+            s._points.clear();
+        }
+        else {
+            std::stringstream ss(l);
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            char comma = 0;
+            ss >> x;
+            ss >> comma;
+            ss >> y;
+            ss >> comma;
+            ss >> z;
+            s._points.emplace_back(x, y, z);
+        }
+    }
+    scanners.push_back(s);
+
+    return scanners;
+}
+
+void setVectors(std::vector<Scanner>& scanners) {
+    for (auto& s : scanners)
+    {
+        for (auto it = s._points.begin(); it != s._points.end() - 1; ++it) {
+            for (auto it2 = it + 1; it2 != s._points.end(); ++it2)
+                s._vectorsBetweenPoints.emplace_back(*(it2)-*it);
+        }
+    }
+}
 
 int main() {
+    auto scanners = readData("../data/day19_test.txt");
+    setVectors(scanners);
 
+    testInverse();
     return 0;
 }
